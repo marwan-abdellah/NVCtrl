abfc8982120062d02e68d62b4395c43d30561e73
Marwan Abdellah <abdellah.marwan@gmail.com>
2012-05-03 11:27:07 +0200

    Updating copyrights

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -1,37 +1,35 @@
-# @author  Marwan Abdellah
-# @remarks Copyright (c) BBP/EPFL 2011; All rights reserved. Do not distribute without further notice.
+# -----------------------------------------------------------------------------
+# Author: Marwan Abdellah <marwan-abdellah@epfl.ch>
+# Blue Brain Project, Ecole Polytechnique Federal Du Lausanne (EPFL)
+# Copyright (c) BBP/EPFL 2011-2012.  
+# All rights reserved. 
 
 # General CPack configuration 
 # Info: http://www.itk.org/Wiki/CMake:Component_Install_With_CPack
-# _____________________________________________________________________________
+# -----------------------------------------------------------------------------
 
+# Package Name 
 set(CPACK_PACKAGE_NAME "NVCtrl")
 
-if(APPLE)
-  set(CPACK_PACKAGE_VENDOR "Blue Brain Project") # PackageMaker doesn't like http://
-else()
-  set(CPACK_PACKAGE_VENDOR "http://bluebrain.epfl.ch") # deb lintian insists on URL
-endif()
+# Vendor 
+set(CPACK_PACKAGE_VENDOR "NVIDIA") # deb lintian insists on URL
 
+# Summary 
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "NVIDIA Control Library ")
+
+# Maintainer 
 set(CPACK_PACKAGE_CONTACT "Marwan Abdellah <marwan.abdellah@epfl.ch>")
 
-
-#set(CPACK_PACKAGE_VERSION ${VERSION})
-#set(CPACK_PACKAGE_VERSION_MAJOR ${VERSION_MAJOR})
-#set(CPACK_PACKAGE_VERSION_MINOR ${VERSION_MINOR})
-#set(CPACK_PACKAGE_VERSION_PATCH ${VERSION_PATCH})
+# Package version 
+set(CPACK_PACKAGE_VERSION ${VERSION})
+set(CPACK_PACKAGE_VERSION_MAJOR 1)
+set(CPACK_PACKAGE_VERSION_MINOR 0)
+set(CPACK_PACKAGE_VERSION_PATCH 0)
 
 # Select package generator
 if(MSVC)
   set(CPACK_GENERATOR "NSIS")
 endif(MSVC)
-
-if(APPLE)
-  set(CPACK_GENERATOR "PackageMaker")
-  set(CPACK_SET_DESTDIR ON)
-  set(CPACK_OSX_PACKAGE_VERSION "10.5")
-endif(APPLE)
 
 if (${CMAKE_SYSTEM_NAME} MATCHES Linux)
   find_program(RPM_EXE rpmbuild)
@@ -43,18 +41,15 @@ if (${CMAKE_SYSTEM_NAME} MATCHES Linux)
 endif()
 
 # Debian specific configuration (minimum)
-# _____________________________________________________________________________
-
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_CONTACT}")
 
-# To be set later ... 
+# X11 dependency 
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libx11-dev")
 
 # RPM specific configuration (minimum)
-# _____________________________________________________________________________
 set(CPACK_RPM_PACKAGE_LICENSE "LGPL")
-set(CPACK_RPM_PACKAGE_GROUP "Sciences/Biology")
-#set(CPACK_RPM_PACKAGE_VERSION ${VERSION})
+set(CPACK_RPM_PACKAGE_GROUP "Miscellaneous - Graphica")
+set(CPACK_RPM_PACKAGE_VERSION ${VERSION})
 
 include(InstallRequiredSystemLibraries)
 include(CPack)
